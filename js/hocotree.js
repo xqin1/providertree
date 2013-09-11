71875b7082a6d9d944dd38219506ad5bbad03d7b
xqin1 <qinxm2000@yahoo.com>
2013-09-11 13:49:47 -0400

    highlight state link

--- js/hocotree.js
+++ js/hocotree.js
@@ -1,13 +1,17 @@
 var treedata = {};
-var hocoParas,stateParas;
-var hocodata, hocokey;
+var hocodata, hocokey, statename;
 var  dbaToolTip = CustomTooltip("dba_tooltip", 200);
 var popFormat = d3.format(",.1s");
+var svg,tree,animGroup;
+
+//getting from PHP
+var hocoParas = 'Verizon Communications Inc.'; //HOCO NAME
+var stateParas = '42'; //Stae Fips or 'all'
 
 var paras=window.location.href.split("?");
 //console.log(paras);
 if (paras.length==0){
-	alert("No HOCO or STATE selected");
+	//alert("No HOCO or STATE selected");
 }
 else{
   var para = paras[1].split("/");
@@ -15,7 +19,7 @@ else{
 	stateParas=decodeURI(para[3]);;//state fips
 }
 
-var margin = {top: 20, right: 120, bottom: 20, left: 280},
+var margin = {top: 20, right: 120, bottom: 20, left: 450},
     width = parseInt(d3.select('#treeGraph').style('width')),
     width = width - margin.right - margin.left,
     mapRatio = 0.9
@@ -25,44 +29,47 @@ var i = 0,
     duration = 750,
     root;
 
-var tree = d3.layout.tree()
-    .size([height, width]);
+// var tree = d3.layout.tree()
+//     .size([height, width]);
 
 var diagonal = d3.svg.diagonal()
     .projection(function(d) { return [d.y, d.x]; });
 
-var svg = d3.select("#treeGraph").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("id", "layout")
-     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// var svg = d3.select("#treeGraph").append("svg")
+//     .attr("width", width + margin.right + margin.left)
+//     .attr("height", height + margin.top + margin.bottom)
+//     .append("g")
+//       .attr("id", "layout")
+//      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("svg:clipPath").attr("id", "clipper")
-        .append("svg:rect")
-        .attr('id', 'clip-rect');
+// svg.append("svg:clipPath").attr("id", "clipper")
+//         .append("svg:rect")
+//         .attr('id', 'clip-rect');
 
-    var animGroup = d3.select("#layout").append("svg:g")
-        .attr("id", "animation")
-        .attr("clip-path", "url(#clipper)");
+//     var animGroup = d3.select("#layout").append("svg:g")
+//         .attr("id", "animation")
+//         .attr("clip-path", "url(#clipper)");
 
 
 queue()
 	.defer(d3.csv, "data/hocodata.csv")
+  .defer(d3.csv, "data/statename.csv")
 	.await(ready);
 
-function ready(error, hoco){
+function ready(error, hoco, state){
 	if (error) throw error;
+  statename = d3.nest().key(function(d){return d.statefips}).map(state);
 	//var hocodata, hocokey;
-	if (stateParas == "all"){
-		hocodata = hoco.filter(function(d){return d.HoCoName == hocoParas});
-	}else{
-		hocodata = hoco.filter(function(d){return d.HoCoName == hocoParas && d.StateFIPS == stateParas});
-	}
+  hocodata = hoco.filter(function(d){return d.HoCoName == hocoParas});
+	// if (stateParas == "all"){
+	// 	hocodata = hoco.filter(function(d){return d.HoCoName == hocoParas});
+	// }else{
+	// 	hocodata = hoco.filter(function(d){return d.HoCoName == hocoParas && d.StateFIPS == stateParas});
+	// }
 	hocodata.forEach(function(d){
-		d.HoCoName = d.HoCoName;//.toUpperCase().toProperCase();
-		d.ProvName = d.ProvName;//.toUpperCase().toProperCase();
-		d.DBAName = d.DBAName;//.toUpperCase().toProperCase();
+		d.HoCoName = d.HoCoName.toUpperCase().toProperCase();
+		d.ProvName = d.ProvName.toUpperCase().toProperCase();
+		d.DBAName = d.DBAName.toUpperCase().toProperCase();
 		d.Pop_Served = +d.Pop_Served;
 	});
 
@@ -83,6 +90,7 @@ function ready(error, hoco){
 
 	treedata.name = hocoParas;
 	treedata.children = [];
+  treedata.numOfDBA = 0;
 	hocoKey.forEach(function(d){
 	    var provObj={};
 	    provObj.name = d.key;
@@ -93,9 +101,35 @@ function ready(error, hoco){
 	      dbaObj.Pop_Served = v.values.Pop_Served;
 	      dbaObj.StateFIPS = v.values.StateFIPS;
 	      provObj.children.push(dbaObj);
+        treedata.numOfDBA++;
 	    })
 	   treedata.children.push(provObj);
 	})
+  //define height
+  height = treedata.numOfDBA < 5 ? 150 :
+            treedata.numOfDBA < 10 ? 250 :
+              treedata.numOfDBA < 15? 350 : 
+                treedata.numOfDBA < 20? 450: 500
+                 // treedata.numOfDBA < 25? 550: 650;
+  tree = d3.layout.tree()
+    .size([height, width]);
+
+
+ svg = d3.select("#treeGraph").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("id", "layout")
+     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("svg:clipPath").attr("id", "clipper")
+        .append("svg:rect")
+        .attr('id', 'clip-rect');
+
+   animGroup = d3.select("#layout").append("svg:g")
+        .attr("id", "animation")
+        .attr("clip-path", "url(#clipper)");
+
 	if (treedata.children.length == 0){
 
 	}else{
@@ -111,6 +145,7 @@ function ready(error, hoco){
   	//  }
 
   	//root.children.forEach(collapse);
+
   	update(treedata);
 	}
 }
@@ -179,8 +214,32 @@ function update(source) {
 
   // Enter any new links at the parent's previous position.
   link.enter().insert("path", "g")
-      .attr("class", "link")
+      //.attr("class", "link")
+      .attr("class", function(d){
+        if (d.target.depth == 2){ 
+            if (d.target.StateFIPS.indexOf(stateParas) != -1){
+              return "link selected";
+            }
+            else{
+              return "link";
+            }
+        }
+        else if (d.target.depth == 1){
+            if (typeof d.target.children != 'undefined'){
+              if (d.target.children != null){
+                var isStateLink = false;
+                d.target.children.forEach(function(c){
+                  if (c.StateFIPS.indexOf(stateParas) != -1){
+                    isStateLink = true;
+                  }
+                })
+                return isStateLink ? "link selected" : "link";
+              }
+            }
+        }
+      })
       .attr("d", function(d) {
+        //console.log(d.target)
         var o = {x: source.x0, y: source.y0};
         return diagonal({source: o, target: o});
       });
@@ -208,23 +267,30 @@ function update(source) {
 
 // Toggle children on click.
 function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
+  if (d.depth != 0){
+      if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+      update(d);
   }
-  update(d);
+
 }
 
 function mouseover(d){
   if (d.depth == 2){
-    console.log (d);
+   //console.log (d);
+   var sn = [];
+   d.StateFIPS.forEach(function(s){
+      sn.push(statename[s][0].statenameabbr);
+   })
     var content = "";
     content += "<span class='title'>DBA: </span><span class='title'>" + d.name + "</span></br>";
     content += "<span class='name'>Population Served: </span><span class='value'>" + popFormat(d.Pop_Served) + "</span></br>";
-    content += "<span class='name'>State Served: </span><span class='value'>" + d.StateFIPS.join(' ') + "</span>";
+    content += "<span class='name'>State Served: </span><span class='value'>" + sn.join(', ') + "</span>";
     // content += "<span class='name'>Action: </span><span class='value'>" + d.action + "</span>";
     // content += "<span class='separator'>&nbsp;|&nbsp;</span>";
     // content += "<span class='name'>Cost: </span><span class='value'>" + formatMoney(d.result_payment) + "</span>";
@@ -240,6 +306,13 @@ function mouseover(d){
     // Get the matched links
    var matchedLinks=[]
     svg.selectAll('path.link').filter(function(d){
+         ancestors.forEach(function(p){
+            if (p === d.target){
+                matchedLinks.push(d)
+            }
+        })
+    })
+    svg.selectAll('path.selectedState').filter(function(d){
          ancestors.forEach(function(p){
             if (p === d.target){
                 matchedLinks.push(d)
@@ -295,7 +368,7 @@ String.prototype.toProperCase = function() {
     var aStr = this.split(' ');
     var aProp = [];
     for (str in aStr) {
-        aProp.push(aStr[str].charAt(0).toUpperCase() + aStr[str].slice(1).toLowerCase());
+      aStr[str] != "LLC" ? aProp.push(aStr[str].charAt(0).toUpperCase() + aStr[str].slice(1).toLowerCase()) : aProp.push(aStr[str]);
     }
     return aProp.join(' ');
 };
